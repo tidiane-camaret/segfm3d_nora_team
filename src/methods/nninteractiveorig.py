c7ba4f1563e767faec5dbd68db39233a13d295f2
Tidiane <camaret.tidiane@gmail.com>
2025-05-20 22:18:02 +0200

    nninteractiveorig : add option to include every past interaction before a given prediction

--- src/methods/nninteractiveorig.py
+++ src/methods/nninteractiveorig.py
@@ -55,7 +55,7 @@ class nnInteractiveOrigPredictor:
         is_bbox_iteration=True,  # True if current bbox iteration, False if click iteration
         prev_pred=None,  # Full prediction from previous step (NumPy ZYX)
         num_classes_max=None,  # Optional: limit number of classes processed
-        add_previous_interactions=False,  # Optional: add previous interaction to the current one
+        add_previous_interactions=True,  # Optional: add previous interaction to the current one
     ):
         """
         Predicts the segmentation of the input image using nnInteractive for every class described by the bbox or click list. Called at each iteration step.
@@ -73,86 +73,117 @@ class nnInteractiveOrigPredictor:
             final_segmentation (numpy.ndarray): Final segmentation result.
             inference_time (float): Time taken for inference.
         """
+        
+
         start_time = time.time()
+        clicks, clicks_order = clicks # nn code logic from  https://github.com/MIC-DKFZ/nnInteractive/blob/9192ecf75f2302c4efc7d7657a083c8e967b2ad8/nnInteractive/inference/cvpr2025_challenge_baseline/predict.py
         with torch.autocast(device_type=str(self.device)):
+            
             self.session.set_image(image[None].astype(np.float32))
             target_buffer = torch.zeros(image.shape, dtype=torch.uint8, device='cpu')
             self.session.set_target_buffer(target_buffer)
             result = torch.zeros(image.shape, dtype=torch.uint8)
             
-            num_objects = len(bboxs) if bboxs is not None else len(clicks[1])
-            
-            # Validate bbox and clicks lengths if both are provided
-            if bboxs is not None and clicks is not None:
-                assert len(bboxs) == len(clicks[1]), ('Both bboxs and clicks lists are provided but with different length '
-                                                    'suggesting different number of objects. This is not supported by this script '
-                                                    'and it was not communicated by the organizing team that such cases exist '
-                                                    'or how they are supposed to be handled.')
-
+            num_objects = len(bboxs) if bboxs is not None else len(clicks)
             if bboxs is None and clicks is None:
                 self.log("   Warning: No prompts provided. Returning previous prediction.")
                 return prev_pred.copy(), time.time() - start_time
+            elif is_bbox_iteration:
+                num_classes_prompted = len(bboxs)
+                self.log(f"   Mode: BBox, Classes Prompted: {num_classes_prompted}")
+            else:
+                num_classes_prompted = len(clicks[1])
+                self.log(f"   Mode: Clicks, Classes Prompted: {num_classes_prompted}")
+
+
+            # Determine number of classes to process
+            num_classes = (
+                num_classes_prompted
+                if num_classes_max is None
+                else min(num_classes_max, num_classes_prompted)
+            )
+            self.log(f"Processing {num_classes} classes.")
 
             # Iteration through classes
             for oid in range(1, num_objects + 1):
                 self.log(f"image shape: {image.shape}")
                 self.log(f"\n--- Processing Class Index: {oid} ---")
-                
-                # Reset or add previous prediction
                 if prev_pred is not None:
                     self.session.add_initial_seg_interaction((prev_pred == oid).astype(np.uint8), run_prediction=False)
                 else:
                     self.session.reset_interactions()
 
-                # Process bbox if in bbox iteration
                 if (bboxs is not None) and is_bbox_iteration:
                     bbox_here = bboxs[oid - 1]
                     bbox_here = [
                         [bbox_here['z_min'], bbox_here['z_max'] + 1],
                         [bbox_here['z_mid_y_min'], bbox_here['z_mid_y_max'] + 1],
                         [bbox_here['z_mid_x_min'], bbox_here['z_mid_x_max'] + 1]
-                    ]
-                    with contextlib.redirect_stdout(open(os.devnull, 'w')) if not self.verbose else contextlib.nullcontext():
+                        ]
+                
+                    with contextlib.redirect_stdout(open(os.devnull, 'w')) if not self.verbose else contextlib.nullcontext(): # Suppress output
                         self.log(f"   BBox coordinates: {bbox_here}")
                         self.session.add_bbox_interaction(bbox_here, include_interaction=True, run_prediction=False)
 
-                # Process clicks if in click iteration
-                elif clicks is not None:
+
+                else:  # click iteration TODO : Now, we add all click in temporal order, adding 
                     self.log("CLICK ITERATION")
-                    clicks_here = clicks[0][oid - 1]
-                    clicks_order_here = clicks[1][oid - 1]
-                    fg_ptr = bg_ptr = 0
-                    
-                    for kind in clicks_order_here:
-                        if kind == 'fg':
-                            click = clicks_here['fg'][fg_ptr]
-                            fg_ptr += 1
-                        else:
-                            click = clicks_here['bg'][bg_ptr]
-                            bg_ptr += 1
+                    if clicks is not None:
 
-                        self.log(f"Class {oid}: {kind} click at {click}")
-                        with contextlib.redirect_stdout(open(os.devnull, 'w')) if not self.verbose else contextlib.nullcontext():
-                            self.session.add_point_interaction(click, include_interaction=kind == 'fg', run_prediction=False)
+                        clicks_here = clicks[oid - 1]
+                        clicks_order_here = clicks_order[oid - 1]
+                        fg_ptr = bg_ptr = 0
+                        
+                        for i_click, kind in enumerate(clicks_order_here):
+                            if kind == 'fg':
+                                click = clicks_here['fg'][fg_ptr]
+                                fg_ptr += 1
+                            else:
+                                click = clicks_here['bg'][bg_ptr]
+                                bg_ptr += 1
 
-                # Run prediction
+                            print(f"Class {oid}: {kind} click at {click}")
+                            is_last_click = i_click == (len(clicks_order_here) - 1)
+                            with contextlib.redirect_stdout(open(os.devnull, 'w')) if not self.verbose else contextlib.nullcontext():
+                                if add_previous_interactions or is_last_click:
+                                    self.session.add_point_interaction(click, include_interaction=kind == 'fg', run_prediction=False)
+
+                    #clicks_cls, clicks_order = clicks[0][oid - 1], clicks[1][oid - 1]
+
+                    # ordered_clicks = recover_click_sequence(clicks_cls, clicks_order)
+                    # for click_idx, oc in enumerate(ordered_clicks):
+                    #     is_last_click = click_idx == len(ordered_clicks) - 1
+                    #     click_type = oc["type"]
+                    #     click_coords = oc["coords"]
+                    #     self.log(f"   Adding {click_type} click at {click_coords}")
+                    #             # Add the click to the interaction
+                    #             self.session.add_point_interaction(
+                    #                 click_coords, 
+                    #                 include_interaction=click_type == "fg",
+                    #                 run_prediction=False, 
+                    #             )
+    
+
                 self.session.verbose = self.verbose
                 self.session.new_interaction_centers = [self.session.new_interaction_centers[-1]]
                 self.session.new_interaction_zoom_out_factors = [self.session.new_interaction_zoom_out_factors[-1]]
                 with contextlib.redirect_stdout(open(os.devnull, 'w')) if not self.verbose else contextlib.nullcontext():
                     self.session._predict()
+                forward_pass_count = self.session.forward_pass_count
+                # get the prediction, add it to the final segmentation map
+
                 
-                # Update result
                 result[self.session.target_buffer > 0] = oid
+                if self.device == "cuda":
+                    torch.cuda.empty_cache()
+                    torch.cuda.reset_peak_memory_stats()
+                    empty_cache(torch.device('cuda', 0))
+                gc.collect()
 
-            # Cleanup
             self.session._reset_session()
-            if self.device == "cuda":
-                empty_cache(torch.device('cuda', 0))
 
-            prediction_metrics = {
-                "infer_time": time.time() - start_time,
-                "forward_pass_count": self.session.forward_pass_count
-            }
+            prediction_metrics = {"infer_time": time.time() - start_time,
+                    "forward_pass_count": forward_pass_count}
                 
             return result.cpu().numpy(), prediction_metrics
+    
